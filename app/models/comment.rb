5c131abae86bbd83971e90ddbc514c42d7e7c588
david30-maker <davidokpah396@gmail.com>
2023-06-22 10:46:51 +0100

    create methods fprororr user, comment, post and like models classes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,17 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_create :increment_post_comments_counter
+  after_destroy :decrement_post_comments_counter
+
+  private
+
+  def increment_post_comments_counter
+    post.increment!(:comments_count)
+  end
+
+  def decrement_post_comments_counter
+    post.decrement!(:comments_count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,34 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  after_create :increment_user_posts_counter
+  after_destroy :decrement_user_posts_counter
+
+  after_create :update_likes_counter
+  after_destroy :update_likes_counter
+
+  private
+
+  def increment_user_posts_counter
+    user.increment!(:posts_count)
+  end
+
+  def decrement_user_posts_counter
+    user.decrement!(:posts_count)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(1)
+  end
+
+  def update_comments_counter
+    self.comments_count = comments.count
+    save
+  end
+
+  def update_likes_counter
+    self.likes_count = likes.count
+    save
+  end
 end
