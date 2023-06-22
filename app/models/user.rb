d129fd76318b59cb7185457aa08e9244b5a10b1b
david30-maker <davidokpah396@gmail.com>
2023-06-22 14:44:37 +0100

    fix linter error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
 
-
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
