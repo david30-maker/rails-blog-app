b825dc303e0337afe6e4c426b8b9e995cd489dfb
david30-maker <davidokpah396@gmail.com>
2023-06-29 14:05:37 +0100

    fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
     author_id = params[:author_id]
     @posts = @posts.where(author_id:) if author_id.present?
     @comments = @posts.map(&:recent_comments)
-    @comments =Comment.all
+    @comments = Comment.all
     render :index
   end
 
@@ -26,22 +26,23 @@ class PostsController < ApplicationController
     @post = Post.new
     render :new
   end
-  
+
   def create
     @user = @current_user
     @post = Post.new(post_params)
     @post.author_id = @user.id
-  
+
     if @post.save
-      flash[:success] = "Post successfully created"
+      flash[:success] = 'Post successfully created'
       redirect_to user_posts_url(@post.author_id)
     else
-      flash[:alert] = "Post could not be created"
+      flash[:alert] = 'Post could not be created'
       render :new
     end
   end
 
   private
+
   def post_params
     params.require(:post).permit(:title, :text).merge(author_id: @user.id)
   end
