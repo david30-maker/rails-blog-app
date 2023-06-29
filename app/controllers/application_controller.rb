8f40b282c2b272f06823c7e833fe4c9a94391940
david30-maker <davidokpah396@gmail.com>
2023-06-29 14:02:50 +0100

    add form comment and styling all the commponents repecetively

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-    
+    before_action :current_user
 
     def current_user
-        @current_user || = User.first
-        # @current_user ||= User.find_by(id: session[:user_id])
-    end
+        @current_user ||= User.first
+      end
+      
 end
