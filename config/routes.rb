7d6405d7d9adf370b49408011191f3f16724bab1
david30-maker <davidokpah396@gmail.com>
2023-07-05 23:30:32 +0100

    fix linter error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
+
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
