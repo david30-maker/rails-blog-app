2a5127875cc254194bb7b1730d28cd24cc2ee3d0
Lengushuru Charles <charleslengushuru@gmail.com>
2023-07-07 13:44:12 +0300

    Update routes.rb  to include comments route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:index, :show] do
       resources :posts, only: [:index, :show, :new, :create, :destroy] do
-        # resources :comments, only: [:index, :new, :create, :destroy]
+        resources :comments, only: [:index, :new, :create, :destroy]
         resources :likes, only: [:create]
      end
     end
